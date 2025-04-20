5ec75686fca1a033023bd37bd5bcf5a6e11bd39d
Deepakb769 <deepakbohora01@gmail.com>
2025-04-20 18:44:53 +0530

    Feedback for CometChat Visual Builder submitted

---
+++ my-chats-app/src/CometChat/builderSettings.ts
@@ -0,0 +1,83 @@
+
+        export const BuilderSetting = {
+  "chatFeatures": {
+    "coreMessagingExperience": {
+      "typingIndicator": true,
+      "threadConversationAndReplies": true,
+      "photosSharing": true,
+      "videoSharing": true,
+      "audioSharing": true,
+      "fileSharing": true,
+      "editMessage": true,
+      "deleteMessage": true,
+      "messageDeliveryAndReadReceipts": true,
+      "userAndFriendsPresence": true
+    },
+    "deeperUserEngagement": {
+      "mentions": true,
+      "reactions": true,
+      "messageTranslation": true,
+      "polls": true,
+      "collaborativeWhiteboard": true,
+      "collaborativeDocument": true,
+      "voiceNotes": true,
+      "emojis": true,
+      "stickers": true,
+      "userInfo": true,
+      "groupInfo": true
+    },
+    "aiUserCopilot": {
+      "conversationStarter": true,
+      "conversationSummary": true,
+      "smartReply": true
+    },
+    "groupManagement": {
+      "createGroup": true,
+      "addMembersToGroups": true,
+      "joinLeaveGroup": true,
+      "deleteGroup": true,
+      "viewGroupMembers": true
+    },
+    "moderatorControls": {
+      "kickUsers": true,
+      "banUsers": true,
+      "promoteDemoteMembers": true
+    },
+    "privateMessagingWithinGroups": {
+      "sendPrivateMessageToGroupMembers": true
+    }
+  },
+  "callFeatures": {
+    "voiceAndVideoCalling": {
+      "oneOnOneVoiceCalling": true,
+      "oneOnOneVideoCalling": true,
+      "groupVideoConference": true,
+      "groupVoiceConference": true
+    }
+  },
+  "style": {
+    "theme": "dark",
+    "color": {
+      "brandColor": "#d652b1",
+      "primaryTextLight": "#c14c4c",
+      "primaryTextDark": "#c7bebe",
+      "secondaryTextLight": "#b4b4a400",
+      "secondaryTextDark": "#989898"
+    },
+    "typography": {
+      "font": "inter",
+      "size": "comfortable"
+    }
+  },
+  "layout": {
+    "withSideBar": true,
+    "tabs": [
+      "chats",
+      "calls",
+      "users",
+      "groups"
+    ],
+    "chatType": "user"
+  }
+};
+      
